7935e5b01f0fb88094f22a40fac7ae659f478523
81x08 <nixon133@mail.ru>
2018-10-30 00:29:25 +0500

    Добавлен Commands.class для описания названия команд

---
+++ src/main/java/com/ras/teleskabot/telegram/settings/Commands.java
@@ -0,0 +1,15 @@
+package com.ras.teleskabot.telegram.settings;
+
+public class Commands {
+
+    public static final String initCharCommand = "/";
+
+    public static final String startCommand = initCharCommand + "start";
+    public static final String helpCommand = initCharCommand + "help";
+
+    public static final String announcementCommand = "Анонсы";
+    public static final String intensiveCommand = "Интенсивы";
+    public static final String informationCitiesCommand = "Информация по городам";
+    public static final String workWithBotCommand = "Работа с ботом";
+
+}
